bee7268346b6f14340396600843b384592c8c75a
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-02-05 10:18:43 +0100

    ...

---
+++ components/PostWidget.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, [slug]);
+  console.log(relatedPosts);
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              className="align-middle rounded-full w-16 h-16 object-cover"
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <Link href={`/post/${post.slug}`} className="text-md">
+              {post.title}
+            </Link>{" "}
+            <p className="text-gray-500 text-xs">
+              {moment(post.date).format("MMM DD, YYYY")}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostWidget;
